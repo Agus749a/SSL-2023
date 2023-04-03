d00753381e73dbaacae24741cc9d04f5e974a69e
Agustin Pacheco <103228935+Agus749a@users.noreply.github.com>
2023-04-03 12:45:59 -0300

    Add files via upload

---
+++ HelloC.c
@@ -0,0 +1,11 @@
+#include <stdio.h> //standard input-output header,cabecera que contiene definiciones de las macros, las constantes
+                   //las declaraciones de la biblioteca estándar de C
+
+int main (void) //funcion main, void como argumento. 
+                //int main () e int main (void) en c++ es lo mismo. En cambio en C, no. 
+                //En C, si una funcion no tiene argumento, le puedo pasar inumerables parametros.
+                //en C++ si una funcion no recibe la cantidad de parametros declarada dara error.
+                //Para restringir eso, usamos void como argumento. 
+{
+    printf("Hello World"); 
+}
